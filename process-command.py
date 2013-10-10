5fdf00c83ddf2fb630c9dd2a960b9cbbccda10e7
Luke Karrys <lukekarrys@gmail.com>
2013-10-09 17:26:12 -0700

    correctly process resp codes

--- process-command.py
+++ process-command.py
@@ -5,8 +5,10 @@ import notification
 import settings
 import api
 import server
+import requests
 
 user_settings = settings.Settings()
+n = notification.Notification()
 
 param_str = sys.argv[1]
 params = param_str.split(':')
@@ -25,22 +27,30 @@ if command == 'teams':
     sys.exit()
 
 if len(params) == 3:
+    verb = ''
     if command == 'ship':
-        r = api.method('/tasks/' + params[2] + '/ship', params[1], {}, 'post').json()
-        n.notify("Task was shipped", r["title"], r["id"])
+        r = api.method('/tasks/' + params[2] + '/ship', params[1], {}, 'post')
+        verb = 'shipped'
     elif command == 'later':
-        r = api.method('/tasks/' + params[2] + '/later', params[1], {}, 'post').json()
-        n.notify("Task was latered", r["title"], r["id"])
+        r = api.method('/tasks/' + params[2] + '/later', params[1], {}, 'post')
+        verb = 'latered'
     elif command == 'create':
-        r = api.method('/me/tasks', params[1], {'title': params[2]}, 'post').json()
-        n.notify("Task was created", r["title"], r["id"])
+        r = api.method('/me/tasks', params[1], {'title': params[2]}, 'post')
+        verb = 'created'
     elif command == 'activate':
-        r = api.method('/tasks/' + params[2] + '/start', params[1], {}, 'post').json()
-        n.notify("Task was started", r["title"], r["id"])
+        r = api.method('/tasks/' + params[2] + '/start', params[1], {}, 'post')
+        verb = 'activated'
     elif command == 'delete':
-        r = api.method('/tasks/' + params[2], params[1], {}, 'delete').json()
-        n.notify("Task was deleted", r["title"], r["id"])
+        r = api.method('/tasks/' + params[2], params[1], {}, 'delete')
+        verb = 'deleted'
     else:
         n.notify("No action was specified", "", "")
+
+    if r.status_code == requests.codes.ok:
+        task = r.json()
+        n.notify('Task was ' + verb, task['title'], task['id'])
+    else:
+        error = r.json()
+        n.notify('Error with task', error['message'], '')
 else:
     n.notify("Number of parameters was incorrect", "", "")
